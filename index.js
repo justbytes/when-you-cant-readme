9f2a0da5bc2950da755bdac64862ef8cdd6fd4b1
Jesse Marino <jessemarino2020@gmail.com>
2022-04-20 17:24:59 -0700

    index.js now creates a readme file

--- index.js
+++ index.js
@@ -1,21 +1,93 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
+const readmeQuestions = () => {
+    return inquirer.prompt ([
 
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+        {
+            type: 'input',
+            message: 'What the title of you project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Write a description of your project.',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'How do you initialize your project?',
+            name: 'init'
+        },
+        {
+            type: 'input',
+            message: 'How do you use your project?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'What are your licenses?',
+            name: 'license'
+        },
+        {
+            type: 'input',
+            message: 'What can a user do if they would like to contribute?',
+            name: 'contributing'
+        }, 
+        {
+            type: 'input',
+            message: 'Want to test?',
+            name: 'test'
+        }, 
+        {
+            type: 'input',
+            message: 'Please give contact info for users with questions.',
+            name: 'questions'
+        },   
+    ]);
+}
 
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+const genReadme = ({title, description, init, usage, license, contributing, test, questions}) =>
 
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+`# ${title}
 
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+## Description
+${description}
 
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+### Installation
+${init}
+
+### Usage
+${usage}
+
+### License
+${license}
+
+### Contributing
+${contributing}
+
+### Tests
+${test}
+
+### Questions
+${questions}
+`
+
+const init = () => {
+    readmeQuestions()
+      // Use writeFileSync method to use promises instead of a callback function
+      .then((answers) => fs.writeFileSync('readme.md', genReadme(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+  };
+  
+init();
