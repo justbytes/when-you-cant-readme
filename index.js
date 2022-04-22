9495f4cd944c097e8c846f2db17ee9ccf876f60f
Jesse Marino <jessemarino2020@gmail.com>
2022-04-21 22:39:10 -0700

    final commit, index.js creates correct readme also added readme for project in HW readme folder

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const readmeQuestions = () => {
         },
         {
             type: 'input',
-            message: 'How do you initialize your project?',
+            message: 'What is the command line to install necessary dependencies? ',
             name: 'init'
         },
         {
@@ -25,9 +25,51 @@ const readmeQuestions = () => {
             name: 'usage'
         },
         {
-            type: 'input',
+            type: 'list',
             message: 'What are your licenses?',
-            name: 'license'
+            name: 'license',
+            choices: [
+                {
+                    name: 'Apache License 2.0',
+                    value:
+                        '[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0) '
+                },
+                {
+                    name: 'BSD 3-Clause license',
+                    value: 
+                        '[![License](https://img.shields.io/badge/License-BSD_3--Clause-orange.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+                },
+                {
+                    name: 'BSD 2-Clause license',
+                    value: 
+                        '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)'
+                },
+                {
+                    name: 'GNU General Public License (GPL)',
+                    value: 
+                        '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+                },
+                {
+                    name: 'GNU Library General Public License (LGPL)',
+                    value: 
+                        '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+                },
+                {
+                    name: 'MIT license',
+                    value: 
+                        '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+                },
+                {
+                    name: 'Unlicense',
+                    value: 
+                        '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+                },
+                {
+                    name: 'Open Database License',
+                    value:
+                         '[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)'
+                },
+            ],
         },
         {
             type: 'input',
@@ -36,7 +78,7 @@ const readmeQuestions = () => {
         }, 
         {
             type: 'input',
-            message: 'Want to test?',
+            message: 'What is the code line to run a test?',
             name: 'test'
         }, 
         {
@@ -51,6 +93,8 @@ const genReadme = ({title, description, init, usage, license, contributing, test
 
 `# ${title}
 
+${license}
+
 ## Description
 ${description}
 
@@ -58,24 +102,26 @@ ${description}
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [License](#license)
 - [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
 
 ### Installation
+
+To install necessary dependencies, run the following command
 ${init}
 
 ### Usage
 ${usage}
 
-### License
-${license}
 
-### Contributing
+### Contrnode ibuting
 ${contributing}
 
 ### Tests
+
+To run tests, run the following command:
+
 ${test}
 
 ### Questions
@@ -84,7 +130,6 @@ ${questions}
 
 const init = () => {
     readmeQuestions()
-      // Use writeFileSync method to use promises instead of a callback function
       .then((answers) => fs.writeFileSync('readme.md', genReadme(answers)))
       .then(() => console.log('Successfully wrote to index.html'))
       .catch((err) => console.error(err));
---
+++ readme.md
@@ -0,0 +1,35 @@
+# project
+
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+
+## Description
+this is a description
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+### Installation
+
+To install necessary dependencies, run the following command
+node
+
+### Usage
+aljkdf
+
+
+### Contrnode ibuting
+yes
+
+### Tests
+
+To run tests, run the following command:
+
+no test
+
+### Questions
+me
